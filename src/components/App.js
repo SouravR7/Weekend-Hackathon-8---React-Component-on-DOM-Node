95ddd576f881980fc1dd3361981ccf9497fc65d3
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-17 02:29:31 +0530

    trmr

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import "../styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <p>
-          Now I can render any React component on any DOM node I want using
-          ReactDOM.render
-        </p>
-      </>
-    );
-  }
+function App() {
+  return (
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>
+  );
 }
 
 export default App;
